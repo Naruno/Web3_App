86d6ae24875903c164a3e877a9be75787b7e4ded
Onur Atakan ULUSOY <atadogan06@gmail.com>
2023-08-10 17:57:01 +0300

    Update chat.py

--- src/web3_app/chat/chat.py
+++ src/web3_app/chat/chat.py
@@ -23,7 +23,7 @@ class ChatMessage(ft.Row):
                 ft.Column(
                     [
                         ft.Text(message.user_name, weight="bold"),
-                        ft.Text(message.text, selectable=True),
+                        ft.Text(message.text, width=self.width, selectable=True),
                     ],
                     tight=True,
                     spacing=5,
